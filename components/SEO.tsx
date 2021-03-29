b55d6c282ce884e77209bc5896457de24cde0fde
Sumukha210 <sumukhakb@gmail.com>
2021-03-29 19:38:58 +0530

    seo

---
+++ components/SEO.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const SEO = () => {
+  return (
+    <div>
+      <Head>
+        <title>Lead project</title>
+        <meta name="description" content="lead project using nextjs" />
+        <meta name="keywords" content="nextjs" />
+      </Head>
+    </div>
+  );
+};
+
+export default SEO;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,12 @@ import { Provider } from "react-redux";
 import NavBar from "../components/common/Navbar";
 import "../css/global.css";
 import { store } from "../Redux/store";
+import SEO from "../components/SEO";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <SEO />
       <Provider store={store}>
         <NavBar />
         <Component {...pageProps} />
